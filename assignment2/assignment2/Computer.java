2e409a9852f02e891011c9689ec0bf5ef084967d
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-09-18 20:18:28 -0500

    created the two additional classes necessary for the game

---
+++ assignment2/assignment2/Computer.java
@@ -0,0 +1,5 @@
+package assignment2;
+
+//This will be the home of the Computer's actual Game actions
+public class Computer {
+}
---
+++ assignment2/assignment2/Player.java
@@ -0,0 +1,5 @@
+package assignment2;
+
+//This class will be the home of the players actions
+public class Player {
+}
