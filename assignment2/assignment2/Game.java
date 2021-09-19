79a1430ae20c7f8030a0cff30b864acadd6291d3
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-09-18 20:30:40 -0500

    finished play again routine properly

--- assignment2/assignment2/Game.java
+++ assignment2/assignment2/Game.java
@@ -1,30 +1,44 @@
 package assignment2;
 
 import java.util.Scanner;
+
+//This class is meant to be the Game environment
+//It will function as the "arena" for the player and computer classes
 public class Game {
-    private boolean test;
-    private GameConfiguration conf;
-    private SecretCodeGenerator gen;
-    private Scanner scan;
+    private final boolean test;
+    private final GameConfiguration conf;
+    private final SecretCodeGenerator gen;
+
+    private final Scanner scan;
+    private String code;
     public boolean playAgain;
+
 
     //constructor
     public Game(boolean isTesting, GameConfiguration config, SecretCodeGenerator generator){
         //game constructor
         this.test = isTesting;
-        this.conf = config;
-        this.gen = generator;
-        scan = new Scanner(System.in);
+        this.conf = config; //may not be needed, will delete if that's the case
+
+        this.scan = new Scanner(System.in);
         this.playAgain = true;
+        this.gen = generator;
+        this.code = this.gen.getNewSecretCode();
     }
 
     //main game logic here
     public void runGame(){
         if (this.test) {
             //print the testing stuff
-            System.out.println("testingmode");
+            System.out.println("testing mode");
         }
      //main logic
+        System.out.println("debug run game");
+        System.out.println("Testing:"+this.test);
+        System.out.println(conf);
+        System.out.println("code:"+this.code);
+
+
     }
 
     //prompt user to play the game again and return the boolean whether they want to
@@ -37,8 +51,9 @@ public class Game {
                 System.out.println("Please input either Y or N only.");
             } else if(response == 'Y') {
                 this.playAgain = true;
+                this.code = this.gen.getNewSecretCode();
                 invalid = false;
-            } else if(response == 'N'){
+            } else {
                 this.playAgain = false;
                 invalid = false;
             }
