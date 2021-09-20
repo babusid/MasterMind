38756cb96b0b3f2507af9d91fb0ca8e5d75abecf
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2021-09-19 22:50:35 -0500

    created multiguess system, changed guessEvaluate to boolean return type, created custom response object

--- assignment2/assignment2/Computer.java
+++ assignment2/assignment2/Computer.java
@@ -1,2 +1,43 @@
-package assignment2;public class Computer {
+package assignment2;
+
+import java.text.Format;
+import java.util.ArrayList;
+
+//this object does the secret code evaluation and feedback generation
+//also holds the history of all the guesses that are passed into it
+public class Computer {
+    private final FormattedGuess answer; //the response that is completely correct
+    private final ArrayList<String> evalGuesses = new ArrayList<>(); //all the guesses after they've been evaluated
+
+    public Computer(String code){
+        this.answer = new FormattedGuess(code.length(),0,code);
+    }
+
+    public boolean guessEvaluate(String guess){ //make it return a boolean on whether they have gotten it right.
+        if(guess.equals("HISTORY")){
+            //run the history routine
+            System.out.println("history command");
+            System.out.println(evalGuesses);
+            return false;
+        }else if(guess.equals("TRUECOM")){
+            return true;
+        }
+        System.out.println("not history command");
+        evalGuesses.add(guess);
+        return false;
+    }
+
+}
+
+class FormattedGuess extends Response{
+    public String guess;
+    public FormattedGuess(int b, int w, String userGuess){
+        super(b,w);
+        this.guess = userGuess;
+    }
+
+    //generate String
+    public String feedbackStringGenerate(){
+        return (guess + " -> "+b+"b_"+w+"w");
+    }
 }
--- assignment2/assignment2/Game.java
+++ assignment2/assignment2/Game.java
@@ -6,24 +6,27 @@ import java.util.Scanner;
 //It will function as the "arena" for the player and computer classes
 public class Game {
     private final boolean test;
-    private final GameConfiguration conf;
     private final SecretCodeGenerator gen;
+    private final GameConfiguration config;
 
-    private final Scanner scan;
-    private String code;
-    public boolean playAgain;
-
+    private final Scanner scan; //scanner object
+    public boolean playAgain; //boolean controlling repeated playing
+    private final Player player; //player object
+    private Computer computer; //computer object
+    private Integer guesses; //how many guesses the player gets
 
     //constructor
     public Game(boolean isTesting, GameConfiguration config, SecretCodeGenerator generator){
         //game constructor
         this.test = isTesting;
-        this.conf = config; //may not be needed, will delete if that's the case
+        this.gen = generator;
+        this.config = config;
 
         this.scan = new Scanner(System.in);
         this.playAgain = true;
-        this.gen = generator;
-        this.code = this.gen.getNewSecretCode();
+        this.player = new Player(this.scan);
+        this.computer = new Computer(this.gen.getNewSecretCode());
+        this.guesses = this.config.guessNumber;
     }
 
     //main game logic here
@@ -32,13 +35,24 @@ public class Game {
             //print the testing stuff
             System.out.println("testing mode");
         }
-     //main logic
+
+    //DEBUG
         System.out.println("debug run game");
         System.out.println("Testing:"+this.test);
-        System.out.println(conf);
-        System.out.println("code:"+this.code);
-
-
+        this.gen.printConfig();
+    //END DEBUG
+        while(this.guesses>0){
+            String guess = player.Guess();
+            if(guess.equals("HISTORY")){this.guesses++;}
+            if(computer.guessEvaluate(guess)){
+                break;
+            }
+            guesses--;
+        }
+        if(this.guesses>0){
+            //Win case
+        }
+        //Loss case
     }
 
     //prompt user to play the game again and return the boolean whether they want to
@@ -51,7 +65,8 @@ public class Game {
                 System.out.println("Please input either Y or N only.");
             } else if(response == 'Y') {
                 this.playAgain = true;
-                this.code = this.gen.getNewSecretCode();
+                this.guesses = this.config.guessNumber;
+                this.computer = new Computer(this.gen.getNewSecretCode());
                 invalid = false;
             } else {
                 this.playAgain = false;
